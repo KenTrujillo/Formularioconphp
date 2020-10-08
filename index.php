02aae69ac50cca2ac86b93c417e31f234e7bfd05
Kenia2507 <quizkenia@gmail.com>
2020-10-07 22:46:31 -0600

    Creando un formulario de datos personales

--- index.php
+++ index.php
@@ -6,6 +6,30 @@
     <title>Formulario de recepcion de datos desde html</title>
 </head>
 <body>
+
+<form action="">
+    <h5>Datos personales</h5>
+    <label for="">
+        Nombres:
+    </label>
+    <input type="text" name="nombre"> 
+    <br>
+
+    <label for="">
+        Apellidos:
+    </label>
+    <input type="text" name="apellido"> 
+    <br>
+
+    <label for="">
+        Edad:
+    </label>
+    <input type="number" name="edad"> 
+    <br>
+
+    <button type="submit">Enviar información</button>
+
+</form>
     
 </body>
 </html>
